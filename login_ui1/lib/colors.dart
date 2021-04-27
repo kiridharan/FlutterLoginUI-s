fd94bdb755f0e88bb1356dd50788121b9db4d646
KIRIDHARAN S <kiridharan217dec2001@gmail.com>
2021-04-27 15:37:42 +0530

    added sign up page and naviagtion

--- login_ui1/lib/colors.dart
+++ login_ui1/lib/colors.dart
@@ -3,26 +3,25 @@ import 'package:flutter/material.dart';
 const kBlackHalf = const Color(0xFF212121);
 const kBlackLight = Colors.white;
 const kBlack = const Color(0xFF000000);
-const kYellow = const Color(0xFFffd600);
-const kYellowLight = const Color(0xFFffff52);
-const kYellowDark = const Color(0xFFc7a500);
+
+// const kYellowDark = const Color(0xFFc7a500);
 const kWhite = Colors.white;
 
 ThemeData buildDarkTheme() {
   final ThemeData base = ThemeData();
   return base.copyWith(
     primaryColor: kBlack,
-    accentColor: kYellow,
+    accentColor: kBlackHalf,
     scaffoldBackgroundColor: kBlackHalf,
     primaryTextTheme: buildTextTheme(base.primaryTextTheme, kBlack),
     primaryIconTheme: base.iconTheme.copyWith(color: kWhite),
-    buttonColor: kYellow,
+    buttonColor: kBlackHalf,
     textTheme: buildTextTheme(base.textTheme, kBlack),
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
         borderSide: BorderSide(color: kWhite),
       ),
-      labelStyle: TextStyle(color: kYellow, fontSize: 24.0),
+      labelStyle: TextStyle(color: kBlackHalf, fontSize: 24.0),
     ),
   );
 }
--- login_ui1/lib/main.dart
+++ login_ui1/lib/main.dart
@@ -12,22 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: buildDarkTheme(),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: LoginContainer(),
+      home: LoginContainer(),
     );
   }
 }
@@ -35,29 +20,29 @@ class _MyHomePageState extends State<MyHomePage> {
 class LoginContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Positioned.fill(
-          child: Image(
-            image: AssetImage('assets/images/Bg.png'),
-            fit: BoxFit.fill,
-          ),
-        ),
-        Positioned(
-          top: 80,
-          left: 170,
-          child: Image(
-            height: 90,
-            width: 90,
-            image: AssetImage(
-              'assets/images/Logo.png',
+    return Scaffold(
+      body: Stack(
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          Positioned.fill(
+            child: Image(
+              image: AssetImage('assets/images/Bg.png'),
+              fit: BoxFit.fill,
             ),
           ),
-        ),
-        Positioned(
-          top: 240,
-          child: SingleChildScrollView(
-            reverse: true,
+          Positioned(
+            top: 80,
+            left: 130,
+            child: Image(
+              height: 90,
+              width: 90,
+              image: AssetImage(
+                'assets/images/Logo.png',
+              ),
+            ),
+          ),
+          Positioned(
+            top: 240,
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -70,12 +55,13 @@ class LoginContainer extends StatelessWidget {
               child: Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(70.0),
+                    padding: const EdgeInsets.all(10.0),
                     child: Text(
                       "Login",
                       style: TextStyle(
                         fontSize: 30,
-                        fontWeight: FontWeight.normal,
+                        fontFamily: "OpenSans",
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
@@ -84,44 +70,79 @@ class LoginContainer extends StatelessWidget {
                     obscure: false,
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 30,
                   ),
                   InputForm(
                     inputString: "password",
                     obscure: true,
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 30,
                   ),
                   Button(
+                    buttontext: "Login",
                     onTap: () {
                       print("Login");
                     },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 50, left: 90),
-                    child: Container(
-                      child: Row(
-                        children: [
-                          Text("Don't have any account?"),
-                          GestureDetector(
-                            onTap: () {
-                              print("Sign Up");
-                            },
-                            child: Container(
-                              child: Text("Sign Up"),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextLogin(
+                    heigh: 100.0,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
+                      );
+                    },
+                    signText: "SignUp",
                   )
                 ],
               ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextLogin extends StatelessWidget {
+  final signText;
+  final onTap;
+  final heigh;
+
+  const TextLogin({Key key, this.signText, this.onTap, this.heigh})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: heigh),
+      child: Container(
+        child: Row(
+          children: [
+            Text(
+              "Don't have any account?",
+              style: TextStyle(
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+            GestureDetector(
+              onTap: onTap,
+              child: Container(
+                child: Text(
+                  "$signText",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "OpenSans",
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
@@ -133,9 +154,13 @@ class InputForm extends StatelessWidget {
   const InputForm({Key key, this.inputString, this.obscure}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return Padding(
-      padding: EdgeInsets.only(left: 30, right: 30),
+      padding: EdgeInsets.all(4),
       child: Container(
+        height: 93,
+        width: width / 1.1,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(20),
@@ -157,18 +182,27 @@ class InputForm extends StatelessWidget {
                 inputString,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
+                  fontFamily: "OpenSans",
                   letterSpacing: 0.3,
                 ),
               ),
             ),
-            TextFormField(
-              obscureText: obscure,
-              cursorHeight: 25,
-              cursorColor: Colors.grey,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.only(left: 7),
+              child: TextFormField(
+                obscureText: obscure,
+                cursorHeight: 25,
+                cursorColor: Colors.grey,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    fontSize: 15,
+                    fontFamily: "OpenSans",
+                  ),
+                  hintText: "$inputString",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
               ),
             ),
@@ -180,9 +214,10 @@ class InputForm extends StatelessWidget {
 }
 
 class Button extends StatelessWidget {
+  final buttontext;
   final onTap;
 
-  const Button({Key key, this.onTap}) : super(key: key);
+  const Button({Key key, this.onTap, this.buttontext}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -198,18 +233,149 @@ class Button extends StatelessWidget {
               bottomRight: Radius.circular(15),
             ),
           ),
-          width: 400,
+          width: 320,
           height: 60,
           child: Center(
             child: Text(
-              "Login",
+              "$buttontext",
               style: TextStyle(
+                fontFamily: "OpenSans",
                 color: Colors.white,
                 fontSize: 30,
                 fontWeight: FontWeight.w400,
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: AlignmentDirectional.centerStart,
+          children: [
+            Positioned.fill(
+              child: Image(
+                image: AssetImage('assets/images/Bg.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            Positioned(
+              top: 20,
+              child: Container(
+                // color: Colors.red,
+                width: 500,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      width: 300,
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontFamily: "OpenSans",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 90,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                  ),
+                ),
+                width: width,
+                height: height,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 8,
+                    ),
+                    InputForm(
+                      inputString: "First name",
+                      obscure: false,
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    InputForm(
+                      inputString: "last name",
+                      obscure: true,
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    InputForm(
+                      inputString: "Email",
+                      obscure: true,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    InputForm(
+                      inputString: "password",
+                      obscure: true,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    InputForm(
+                      inputString: "confirm password",
+                      obscure: true,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Button(
+                      buttontext: "Sign Up",
+                      onTap: () {
+                        print("Login");
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextLogin(
+                      heigh: 80.0,
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      signText: "Login",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
